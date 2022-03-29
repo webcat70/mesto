2afdb76a0eb556d187cb117fe5eacfcdad41d67f
webcat <gkrasnyak2@gmail.com>
2022-03-29 21:36:35 +0300

    Место

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const popup = document.querySelector('.popup');
+const buttonTypeEdit = document.querySelector('.button_type_edit');
+const closeButton = document.querySelector('.popup__close-button');
+
+
+
+function openPopup() {
+	popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+	popup.classList.remove('popup_opened');
+}
+
+buttonTypeEdit.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+let popupForm = document.querySelector('.popup__form');
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__text');
+let nameInput = document.querySelector('.popup__input_name_active');
+let jobInput = document.querySelector('.popup__input_job_active');
+
+nameInput.value = name.textContent;
+jobInput.value = job.textContent;
+
+function formSubmitHandler (evt) {
+	evt.preventDefault();
+	name.textContent = nameInput.value;
+	job.textContent = jobInput.value;
+	closePopup()
+}
+
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
+
