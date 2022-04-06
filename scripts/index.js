67d41635e2c53ea33f27488224913f249787ffb4
webcat <gkrasnyak2@gmail.com>
2022-04-06 23:43:58 +0300

    Исправления

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,18 @@ const popup = document.querySelector('.popup');
 const buttonTypeEdit = document.querySelector('.button_type_edit');
 const closeButton = document.querySelector('.popup__close-button');
 
-
-
 function openPopup() {
-	popup.classList.add('popup_opened');
-	nameInput.value = name.textContent;
-   jobInput.value = job.textContent;
+  popup.classList.add('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
 }
-
 
 function closePopup() {
-	popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
-
-
 buttonTypeEdit.addEventListener('click', openPopup);
-
 closeButton.addEventListener('click', closePopup);
-
-
-
 
 let popupForm = document.querySelector('.popup__form');
 let name = document.querySelector('.profile__name');
@@ -30,15 +21,12 @@ let job = document.querySelector('.profile__text');
 let nameInput = document.querySelector('.popup__input_name_active');
 let jobInput = document.querySelector('.popup__input_job_active');
 
-
-
 function formSubmitHandler (evt) {
-	evt.preventDefault();
-	name.textContent = nameInput.value;
-	job.textContent = jobInput.value;
-	closePopup()
+  evt.preventDefault();
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  closePopup()
 }
-
 
 popupForm.addEventListener('submit', formSubmitHandler);
 
